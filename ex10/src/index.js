9e11870da2e80657a3b0d26b8db3f065222bec40
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-12 01:14:38 +0100

    ex10 gotov

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    var myFirstName = "Seid";
+    var myLastName = "Moskva";
+    var myHomeTown = "Tuzla";
+    
+    
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+
+    };
+}
+console.log(main());
+module.exports = main;
